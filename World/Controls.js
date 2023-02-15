81444d1fef0ce6f238f7d4638cd63544629e00cc
alexlato <86857302+alexlato@users.noreply.github.com>
2023-02-14 21:02:16 -0800

    Html, CSS, & Scroll Progress Bar changes

--- World/Controls.js
+++ World/Controls.js
@@ -1,7 +1,8 @@
 import * as THREE from "three";
 import Experience from "../Experience/Experience";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ASScroll from "@ashthornton/asscroll";
 
 export default class Controls {
   constructor() {
@@ -12,9 +13,11 @@ export default class Controls {
     this.time = this.experience.time;
     this.camera = this.experience.camera;
     this.house = this.experience.world.house.actualHouse;
+
     gsap.registerPlugin(ScrollTrigger);
 
     this.setPath();
+    this.setScrollTrigger();
   }
 
   setPath() {
@@ -25,11 +28,73 @@ export default class Controls {
       },
       scrollTrigger: {
         trigger: ".first-move",
-        markers: true,
         start: "top top",
         end: "bottom bottom",
         scrub: 0.8,
         invalidateOnRefresh: true,
+      },
+    });
+  }
+
+  setScrollTrigger() {
+    ScrollTrigger.matchMedia({
+      all: () => {
+        this.sections = document.querySelectorAll(".section");
+        this.sections.forEach((section) => {
+          this.progressWrapper = section.querySelector(".progress-wrapper");
+          this.progressBar = section.querySelector(".progress-bar");
+
+          if (section.classList.contains("right")) {
+            gsap.to(section, {
+              borderTopLeftRadius: 10,
+              scrollTrigger: {
+                trigger: section,
+                start: "top bottom",
+                end: "top top",
+                scrub: 0.6,
+              },
+            });
+            gsap.to(section, {
+              borderBottomLeftRadius: 700,
+              scrollTrigger: {
+                trigger: section,
+                start: "bottom bottom",
+                end: "bottom top",
+                scrub: 0.6,
+              },
+            });
+          } else {
+            gsap.to(section, {
+              borderTopRightRadius: 10,
+              scrollTrigger: {
+                trigger: section,
+                start: "top bottom",
+                end: "top top",
+                scrub: 0.6,
+              },
+            });
+            gsap.to(section, {
+              borderBottomRightRadius: 700,
+              scrollTrigger: {
+                trigger: section,
+                start: "bottom bottom",
+                end: "bottom top",
+                scrub: 0.6,
+              },
+            });
+          }
+          gsap.from(this.progressBar, {
+            scaleY: 0,
+            scrollTrigger: {
+              trigger: section,
+              start: "top top",
+              end: "bottom bottom",
+              scrub: 0.4,
+              pin: this.progressWrapper,
+              pinSpacing: false,
+            },
+          });
+        });
       },
     });
   }
